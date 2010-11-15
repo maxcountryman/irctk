04d8412ac218234dcf29d45c6a50bb018f8ea45c
Max Countryman <max.countryman@gmail.com>
2010-11-15 17:51:01 -0500

    adding SSL support

--- core/irc.py
+++ core/irc.py
@@ -1,78 +1,101 @@
 import gevent
 from gevent import socket
+from gevent.ssl import wrap_socket
 from gevent import sleep
 from gevent import queue
 
-class tcp(object):
-    "handles TCP connections"
+class Tcp(object):
+    '''Handles TCP connections. Timeout is 300 secs.'''
 
     def __init__(self, host, port, timeout=300):
-        self.ibuffer = ''
-        self.obuffer = ''
+        self._ibuffer = ''
+        self._obuffer = ''
         self.iqueue = queue.Queue()
         self.oqueue = queue.Queue()
-        self.socket = self.create_socket()
+        self._socket = self._create_socket()
         self.host = host
         self.port = port
         self.timeout = timeout
 
-    def create_socket(self):
+    def _create_socket(self):
         return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def run(self):
-        self.socket.connect((self.host, self.port))
-        gevent.spawn(self.recv_loop)
-        gevent.spawn(self.send_loop)
+    def connect(self):
+        self._socket.connect((self.host, self.port))
+        gevent.spawn(self._recv_loop)
+        gevent.spawn(self._send_loop)
 
-    def recv_from_socket(self, nbytes):
-        return self.socket.recv(nbytes)
+    def disconnect(self):
+        self._socket.close()
+
+    def _recv_from_socket(self, nbytes):
+        return self._socket.recv(nbytes)
     
-    def recv_loop(self):
+    def _recv_loop(self):
         while True:
-            data = self.recv_from_socket(4096)
-            self.ibuffer += data
-            while '\r\n' in self.ibuffer:
-                line, self.ibuffer = self.ibuffer.split('\r\n', 1)
+            data = self._recv_from_socket(4096)
+            self._ibuffer += data
+            while '\r\n' in self._ibuffer:
+                line, self._ibuffer = self._ibuffer.split('\r\n', 1)
                 self.iqueue.put(line)
                 print line
 
-    def send_loop(self):
+    def _send_loop(self):
         while True:
             line = self.oqueue.get().splitlines()[0][:500]
             print ">>> %r" % line
-            self.obuffer += line.encode('utf-8', 'replace') + '\r\n'
-            while self.obuffer:
-                sent = self.socket.send(self.obuffer)
-                self.obuffer = self.obuffer[sent:]
+            self._obuffer += line.encode('utf-8', 'replace') + '\r\n'
+            while self._obuffer:
+                sent = self._socket.send(self._obuffer)
+                self._obuffer = self._obuffer[sent:]
 
-class IRC(object):
-    "handles the IRC protocol"
+class SslTcp(Tcp):
+    '''SSL wrapper for TCP connections. Timeout is 300 secs.'''
 
-    def __init__(self, server, nick, port=6667, channels=['']):
+    def __init__(self, host, port, timeout=300):
+        Tcp.__init__(self, host, port, timeout)
+
+    def _create_socket(self):
+        return wrap_socket(Tcp._create_socket(self), server_side=False)
+
+    def _recv_from_socket(self, nbytes):
+        return self._socket.read(nbytes)
+
+class Irc(object):
+    '''Handles the IRC protocol. Pass true if using SSL.'''
+
+    def __init__(self, server, nick, port=6667, ssl=False, channels=['']):
         self.server = server
         self.nick = nick
         self.port = port
+        self.ssl = ssl
         self.channels = channels
         self.out = queue.Queue() # responses from the server
-        self.hooks = { "ping": self.pong, "396": self._396, }
-        self.connect()
+        self._hooks = { "ping": self._pong, "396": self._396, }
+        self._connect()
         
-        # parallel event loop(s)
-        self.jobs = [gevent.spawn(self.parse_loop)]
+        # parallel event loop
+        self.jobs = [gevent.spawn(self._parse_loop)]
         gevent.joinall(self.jobs)
 
-    def create_connection(self):
-        return tcp(self.server, self.port)
+    def _create_connection(self):
+        return Tcp(self.server, self.port)
 
-    def connect(self):
-        self.conn = self.create_connection()
-        gevent.spawn(self.conn.run)
-        self.set_nick(self.nick)
+    def _create_ssl_connection(self):
+        return SslTcp(self.server, self.port)
+
+    def _connect(self):
+        if self.ssl is False:
+            self.conn = self._create_connection()
+        else:
+            self.conn = self._create_ssl_connection()
+        gevent.spawn(self.conn.connect)
+        self._set_nick(self.nick)
         sleep(1)
         self.cmd("USER",
                 ['pybot', "3", "*",'Python Bot'])
 
-    def parse_loop(self):
+    def _parse_loop(self):
         while True:
             line = self.conn.iqueue.get()
             trailing = ""
@@ -92,43 +115,43 @@ class IRC(object):
             if trailing:
                 args.append(trailing)
                 
-            event = IRCEvent(command, prefix, args, 5)
+            event = IrcEvent(command, prefix, args, 5)
             try:
-                t = gevent.with_timeout(event.timeout, self.call_hook, event)
+                t = gevent.with_timeout(event.timeout, self._call_hook, event)
             except gevent.Timeout, t:
                 pass
 
     def set_hook(self, hook, func):
         self.hooks[hook] = func
         
-    def call_hook(self, event):
-        if event.hook in self.hooks:
-            self.hooks[event.hook](event)
+    def _call_hook(self, event):
+        if event.hook in self._hooks:
+            self._hooks[event.hook](event)
 
-    def pong(self, event):
+    def _pong(self, event):
         self.cmd("PONG", event.args)
         
     def _396(self, event): # finished connecting, we can join
         for channel in self.channels:
-            self.join(channel)
+            self._join(channel)
 
-    def set_nick(self, nick):
+    def _set_nick(self, nick):
         self.cmd("NICK", [nick])
 
-    def join(self, channel):
+    def _join(self, channel):
         self.cmd("JOIN", [channel])
 
     def cmd(self, command, params=None):
         if params:
             params[-1] = ':' + params[-1]
-            self.send(command + ' ' + ' '.join(params))
+            self._send(command + ' ' + ' '.join(params))
         else:
-            self.send(command)
+            self._send(command)
             
-    def send(self, str):
+    def _send(self, str):
         self.conn.oqueue.put(str)
 
-class IRCEvent(object):
+class IrcEvent(object):
     def __init__(self, hook, source, args, timeout):
         self.hook = hook.lower()
         self.source = source
@@ -136,4 +159,4 @@ class IRCEvent(object):
         self.timeout = timeout
 
 if __name__ == "__main__":
-    bot = IRC('irc.voxinfinitus.net', 'Kaa', 6667, ['#voxinfinitus','#landfill'])
+    bot = Irc('irc.voxinfinitus.net', 'Kaa', 6697, True, ['#voxinfinitus','#landfill'])
