b752ed18b147104c1951ae0c2d2c3e9f9c8050e9
Max Countryman <max.countryman@gmail.com>
2010-11-16 11:31:30 -0500

    fixing imports

--- bot.py
+++ bot.py
@@ -1,14 +1,11 @@
-import core.irc
-from core import *
+from core.irc import Irc
 
 import gevent
 
 if __name__ == '__main__':
 
-    irc = irc.Irc
-
-    bot = lambda : irc('irc.voxinfinitus.net', 'Kaa', 6697, True, ['#voxinfinitus','#landfill'])
-    another_bot = lambda : irc('irc.freenode.net', 'Kaa_', 6667, False, ['#landfill'])
+    bot = lambda : Irc('irc.voxinfinitus.net', 'Kaa', 6697, True, ['#voxinfinitus','#landfill'])
+    another_bot = lambda : Irc('irc.freenode.net', 'Kaa_', 6667, False, ['#landfill'])
     
     jobs = [gevent.spawn(bot),gevent.spawn(another_bot)]
     gevent.joinall(jobs)
