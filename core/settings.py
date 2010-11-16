8b9022f1a9cf60396ae36bcb07577828e46ccd71
pedro3005 <pedrorm2000@gmail.com>
2010-11-15 22:31:06 -0300

    Bla

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,4 @@
-import os
 import os.path
 
-log_dir = os.path.join(os.path.split(os.getcwd())[0], "logs")
+log_dir = os.path.join(os.path.split(os.path.split(os.path.abspath(__file__))[0])[0], "logs")
 
