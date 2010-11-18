b5555ab89662845b44be347ba2d5d00c9bf410ba
Max Countryman <max.countryman@gmail.com>
2010-11-17 20:18:40 -0500

    brrrrrrreak

--- core/irc.py
+++ core/irc.py
@@ -1,6 +1,3 @@
-import logging
-import os.path
-
 import gevent
 from gevent import socket
 from gevent.ssl import wrap_socket
@@ -16,14 +13,15 @@ class Tcp(object):
     '''
 
     def __init__(self, host, port, timeout=300):
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+
         self._ibuffer = ''
         self._obuffer = ''
         self.iqueue = queue.Queue()
         self.oqueue = queue.Queue()
         self._socket = self._create_socket()
-        self.host = host
-        self.port = port
-        self.timeout = timeout
 
     def _create_socket(self):
         return socket.socket()
@@ -36,7 +34,6 @@ class Tcp(object):
 
     def disconnect(self):
         self._socket.close()
-        gevent.killall(jobs)
 
     def _recv_from_socket(self, nbytes):
         return self._socket.recv(nbytes)
@@ -48,10 +45,12 @@ class Tcp(object):
             while '\r\n' in self._ibuffer:
                 line, self._ibuffer = self._ibuffer.split('\r\n', 1)
                 self.iqueue.put(line)
+                print line
 
     def _send_loop(self):
         while True:
             line = self.oqueue.get().splitlines()[0][:500]
+            print '>>> %r' % line
             self._obuffer += line.encode('utf-8', 'replace') + '\r\n'
             while self._obuffer:
                 sent = self._socket.send(self._obuffer)
@@ -69,46 +68,21 @@ class SslTcp(Tcp):
 class Irc(object):
     '''Handles the IRC protocol. Pass true if using SSL.'''
 
-    def __init__(self, server, nick, port=6667, ssl=False, channels=[''], debug=False):
+    def __init__(self, server, nick, port=6667, ssl=False, channels=['']):
         self.server = server
         self.nick = nick
         self.port = port
         self.ssl = ssl
         self.channels = channels
-        self.out = queue.Queue() # responses from the server
-        self._hooks = { 'ping': self._pong, '376': self._376, '396': self._396 }
-        
-        # configure logging
-        self.logger = logging.getLogger("irc")
-        if debug:
-            self.logger.setLevel(logging.DEBUG)
-        else:
-            self.logger.setLevel(logging.ERROR)
-        
-        file_handler = logging.FileHandler(os.path.join(settings.log_dir, self.server) + ".log")
-        file_handler.setLevel(logging.DEBUG)
-        
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.ERROR)
-        
-        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-        
-        file_handler.setFormatter(formatter)
-        console_handler.setFormatter(formatter)
-        
-        self.logger.addHandler(file_handler)
-        self.logger.addHandler(console_handler)
-        # end of logging
-        
-        self._connect()
-        
-        # parallel event loop
-        self.jobs = [gevent.spawn(self._parse_loop)]
+
+        self.jobs = [gevent.spawn(self._connect)]
         gevent.joinall(self.jobs)
 
     def _create_connection(self):
-        transport = SslTcp if self.ssl else Tcp
-        transport(self.server, self.port)
+        if self.ssl is False:
+            return Tcp(self.server, self.port)
+        else:
+            return SslTcp(self.server, self.port)
 
     def _connect(self):
         self.conn = self._create_connection()
@@ -118,50 +92,8 @@ class Irc(object):
         self.cmd('USER',
                 ['pybot', '3', '*','Python Bot'])
 
-    def _parse_loop(self):
-        while True:
-            line = self.conn.iqueue.get()
-            self.logger.debug("recv: {0}".format(line))
-            trailing = ''
-            prefix = ''
-            
-            if line[0] == ':':
-                line = line[1:].split(' ', 1)
-                prefix = line[0]
-                line = line[1]
-            
-            if ' :' in line:
-                line = line.split(' :', 1)
-                trailing = line[1]
-                line = line[0]
-            args = line.split()
-            command = args.pop(0)
-            if trailing:
-                args.append(trailing)
-                
-            event = IrcEvent(command, prefix, args, 5)
-            try:
-                t = gevent.with_timeout(event.timeout, self._call_hook, event)
-            except gevent.Timeout, t:
-                self.logger.exception('Hook call timed out!')
-
-    def set_hook(self, hook, func):
-        self.hooks[hook] = func
-        
-    def _call_hook(self, event):
-        if event.hook in self._hooks:
-            self._hooks[event.hook](event)
-
     def _pong(self, event):
         self.cmd('PONG', event.args)
-
-    def _376(self, event): # finished connecting (freenode)
-        for channel in self.channels:
-            self.join(channel)
-   
-    def _396(self, event): # finished connecting, we can join
-        for channel in self.channels:
-            self.join(channel)
 
     def _set_nick(self, nick):
         self.cmd('NICK', [nick])
@@ -177,12 +109,11 @@ class Irc(object):
             self._send(command)
             
     def _send(self, s):
-        self.logger.debug("send: {0}".format(s))
         self.conn.oqueue.put(s)
 
-class IrcEvent(object):
-    def __init__(self, hook, source, args, timeout):
-        self.hook = hook.lower()
-        self.source = source
-        self.args = args
-        self.timeout = timeout
+if __name__ == '__main__':
+    
+    bot = lambda : Irc('irc.voxinfinitus.net', 'Kaa', 6697, True, ['#voxinfinitus','#landfill'])
+    
+    jobs = [gevent.spawn(bot)]
+    gevent.joinall(jobs)
