0e369b1b3a11c5cd54bd2a0062550c54e7508bb3
Max Countryman <max.countryman@gmail.com>
2010-11-19 19:24:50 -0500

    small fixes

--- bot.py
+++ bot.py
@@ -25,13 +25,17 @@ def greet(bot, parsed):
 #    bot.irc.msg(parsed.trailing, 'We will take care of {0} later..'.format(parsed.nick))
 
 class Bot(object): # don't inherit from Irc, keeps things flat :D
+    '''Instantiates Irc, loops over `Irc.conn.iqueue` and sends data through 
+       `dispatch()`. Pass True if using SSL/TLS.
+    '''
+
     def __init__(self, server, nick, port=6667, ssl=False, channels=[]):
         self.channels = channels
         self.irc = Irc(server, nick, port, ssl)
         self._dispatch_events()
 
     def _dispatch_events(self):
-        while True:
+        while True: # magic loop
             event = self.irc.events.get()
             dispatch(self, event)
 
