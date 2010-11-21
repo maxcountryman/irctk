ca4fd65f332106e04d109a3d9c824897eb364275
pedro3005 <pedrorm2000@gmail.com>
2010-11-21 13:33:06 -0300

    Slight improvement to runner

--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
-import os, sys
+import os
+import sys
 
 import gevent
 import yaml
@@ -17,11 +18,10 @@ if __name__ == '__main__':
     if not os.path.isfile('settings.yaml'):
         print 'Error: Could not find the settings file. Please create `settings.yaml` and fill it accordingly.'
         sys.exit()
-    
-    bots = []
+
     settings_array = yaml.load_all(open('settings.yaml'))
     
-    g = lambda s: Bot(s)
+    spawn_bot = lambda setting: Bot(setting)
 
-    jobs = [gevent.spawn(g, s) for s in settings_array]
+    jobs = [gevent.spawn(spawn_bot, setting) for setting in settings_array]
     gevent.joinall(jobs)
