1965ad99b9d3e5fb9135b60dacebc43981e11ea8
Max Countryman <max.countryman@gmail.com>
2010-11-22 09:24:10 -0500

    updating, this was way old

--- core/hooks.py
+++ core/hooks.py
@@ -1,36 +1,29 @@
 from collections import defaultdict
-import inspect
 
 cmd_hooks = {} # store functions in a standard dictionary
 sub_hooks = defaultdict(list) # store functions in a list associated with a hook
 
 def command(hook=None):
-    '''Associate a command hook with a function. For example `@command('google')`
-       should call a given function with `google` hook, alternatively using 
-       `@command` should use `func.func_name` as the hook. Uses cmd_hooks dict.
-    '''
+    '''Associate a command hook with a function. Uses cmd_hooks dict.'''
 
     def command_wrapper(func):
-        if hook is None and func.func_name in cmd_hooks or hook in cmd_hooks:
+        if hook in cmd_hooks or not hook and func.func_name in cmd_hooks:
             raise ValueError('Duplicate command hook found.')
         
-        if hook is None:
-            cmd_hooks[func.func_name] = func
-        else:
+        if hook:
             cmd_hooks[hook] = func
-            
-            return func
+        else:
+            cmd_hooks[func.func_name] = func
         
-    if hook is not None:
+        return func
+    
+    if hook:
         return command_wrapper
     else:
         return command_wrapper(hook)
 
 def subscribe(hook):
-    '''Subscribe a function or functions to an event. For example 
-       `@subscribe('JOIN')` should call a given function or functions with the 
-       `JOIN` hook. Uses sub_hook dict.
-    '''
+    '''Subscribe a function to an event. Uses sub_hooks dict.'''
 
     def subscribe_wrapper(func):
         sub_hooks[hook].append(func)
