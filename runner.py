2f34df9474583900163c696e7424201e8dd87aba
Max Countryman <max.countryman@gmail.com>
2011-03-31 13:58:28 -0700

    updating runner.py, should actually run now

--- runner.py
+++ runner.py
@@ -1,18 +1,10 @@
-import voxbot
+#!/usr/bin/env python
 import gevent
 
-SETTINGS = {
-    'server': 'irc.voxinfinitus.net', 
-    'nick': 'Kaa', 
-    'realname': 'Kaa the Python', 
-    'port': 6697, 
-    'ssl': True, 
-    'channels': ['#testing'], 
-    'owners': ['doraemon']
-    }
+from voxbot import bot, settings
 
+jobs = [gevent.spawn(bot, settings)]
 try:
-    jobs = [gevent.spawn(voxbot.bot, SETTINGS)]
     gevent.joinall(jobs)
 finally:
     gevent.killall(jobs)
