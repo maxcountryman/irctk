3783d60958815829812f71ca9b88505e3d791667
Max Countryman <maxc@me.com>
2011-08-14 08:31:03 -0400

    adding more tests

--- test_kaa.py
+++ test_kaa.py
@@ -12,12 +12,26 @@ class BotTestCase(unittest.TestCase):
     def test_config(self):
         self.assertTrue(isinstance(self.bot.config, dict))
     
+    def test_command(self):
+        @self.bot.command('test')
+        def test_command():
+            pass
+        self.assertTrue(isinstance(self.bot.config['PLUGINS'], list))
+        for plugin in self.bot.config['PLUGINS']:
+            assert(plugin['hook'])
+            assert(plugin['func'])
+            if plugin['hook'] == 'test':
+                plugin = plugin['func']
+        self.assertTrue(inspect.isfunction(plugin))
+    
     def test_event(self):
         @self.bot.event('JOIN')
-        def on_join():
+        def test_event():
             pass
         self.assertTrue(isinstance(self.bot.config['EVENTS'], list))
         for event in self.bot.config['EVENTS']:
+            assert(event['hook'])
+            assert(event['func'])
             if event['hook'] == 'JOIN':
                 event = event['func']
         self.assertTrue(inspect.isfunction(event))
