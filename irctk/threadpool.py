38e23b54f50d0306d94ea9e94875a8344baf9eda
Max Countryman <maxc@me.com>
2011-09-16 21:03:51 -0400

    thread pool now scales to accommodate plugin calls as they increase

--- irctk/threadpool.py
+++ irctk/threadpool.py
@@ -49,11 +49,11 @@ class ThreadPool(threading.Thread):
     Inherits from `threading.Thread`.
     '''
     
-    def __init__(self, workers, logger, wait=0.1):
+    def __init__(self, workers, logger, max_workers=11, wait=0.1):
         threading.Thread.__init__(self)
         self.tasks = Queue.Queue(workers)
         self.workers = workers
-        self.max_workers = workers
+        self.max_workers = max_workers
         self.number_of_workers = 0
         self.logger = logger
         self.wait = wait
@@ -73,6 +73,15 @@ class ThreadPool(threading.Thread):
             
             for worker in range(self.workers):
                 
-                if self.number_of_workers < self.max_workers:
+                if self.number_of_workers < self.workers:
                     self.number_of_workers += 1
                     self.worker() # spawn a worker
+                
+                more_workers_ok = lambda : self.number_of_workers < self.max_workers
+                if not self.tasks.empty() and more_workers_ok():
+                    self.number_of_workers += 1
+                    self.worker()
+                
+                too_many_workers = lambda : self.number_of_workers > self.workers
+                if self.tasks.empty() and too_many_workers():
+                    self.number_of_workers -= 1
