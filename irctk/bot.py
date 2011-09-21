fe4b8c191ab0900771546876b0940687c5810053
Bart Kroon <bart@tarmack.eu>
2011-09-21 23:33:54 +0200

    Put the reloader code in a separate file.

--- irctk/bot.py
+++ irctk/bot.py
@@ -6,14 +6,13 @@
 '''
 
 import os
-import sys
 import time
 import inspect
 import thread
-import imp
 
 from .config import Config
 from .logging import create_logger
+from .reload import Reload
 from .plugins import PluginHandler
 from .ircclient import TcpClient, IrcWrapper
 
@@ -40,6 +39,7 @@ class Bot(object):
     def __init__(self):
         self.config = Config(self.root_path, self.default_config)
         self.plugin = PluginHandler(self.config, self.logger, self.reply)
+        self.reloader = Reload(self.config, self.logger)
     
     def __new__(cls, *args, **kwargs):
         '''Here we override the `__new__` method in order to achieve a 
@@ -113,87 +113,6 @@ class Bot(object):
                     
                     # we're done here, context is stale, give us fresh fruit!
                     self.irc.context['stale'] = True    
-    
-    def _reloader_loop(self, wait=1.0):
-        '''This reloader is based off of the Flask reloader which in turn is 
-        based off of the CherryPy reloader.
-        
-        This internal method takes on parameter, `wait`, the wait time given 
-        in seconds. The default value is set to one second.
-        '''
-        
-        def iter_module_files():
-            for module in sys.modules.values():
-                filename = getattr(module, '__file__', None)
-                if filename:
-                    old = None
-                    while not os.path.isfile(filename):
-                        old = filename
-                        filename = os.path.dirname(filename)
-                        if filename == old:
-                            break
-                    else:
-                        if filename[-4:] in ('.pyc', '.pyo'):
-                            filename = filename[:-1]
-                        yield filename
-        
-        fnames = []
-        fnames.extend(iter_module_files())
-        self._reloader(fnames, wait=wait)
-    
-    def _reloader(self, fnames, wait=1.0):
-        '''This reloader is based off of the Flask reloader which in turn is 
-        based off of the CherryPy reloader.
-        
-        This internal method takes two paramters, `fnames` and `wait.` Here 
-        we loop over a list of file names, `fnames`, using `os.stat` to 
-        determine if the mtime of the file has been changed. If so we use 
-        `imp.load_source` to reload the module.
-        
-        The `fnames` parameter should be a list of files names to be parsed by 
-        the reloader.
-        
-        A callback function, `callback` may be provided as a function to be 
-        called when the file is reloaded.
-        
-        The `wait` parameter specifies the time in seconds to run 
-        `time.sleep()`.
-        ''' 
-        
-        mtimes = {}
-        
-        while True:
-            for filename in fnames:
-                try:
-                    mtime = os.stat(filename).st_mtime
-                except OSError, e:
-                    self.logger.error('Reloader error: {0}'.format(e))
-                    continue
-                
-                old_time = mtimes.get(filename)
-                if old_time is None:
-                    mtimes[filename] = mtime
-                elif mtime > old_time:
-                    mtimes[filename] = mtime
-                    
-                    self.logger.info('Changes detected; reloading {0}'.format(filename))
-                    
-                    old_lists = self.plugin.flush_plugin_lists()
-                    
-                    f = filename.split('/')[-1].split('.')[0]
-                    try:
-                        imp.load_source(f, filename)
-                    except Exception, e:
-                        self.logger.error('Failed loading plugin: ' + str(e))
-                        continue
-                    finally:
-                        mtimes[filename] = mtime
-                    
-                    if (self.config['PLUGINS'] or self.config['EVENTS']) == []:
-                        self.config['PLUGINS'] = old_lists[0]
-                        self.config['EVENTS'] = old_lists[1]
-                        
-            time.sleep(wait)
     
     def command(self, hook=None, **kwargs):
         '''This method provides a decorator that can be used to load a 
@@ -296,7 +215,7 @@ class Bot(object):
         self.irc.run() 
         
         thread.start_new_thread(self._parse_input, ())
-        thread.start_new_thread(self._reloader_loop, ())
+        self.reloader.run()
         
         while True:
             time.sleep(wait)
