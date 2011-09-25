a1bd1476f135bb4945b55621848dd552f1c6cc7e
Max Countryman <maxc@me.com>
2011-09-25 12:09:22 -0400

    rolling back changes on the thread pool for now
    Using sys.trace ultimately isn't a good solution for this problem.

--- irctk/threadpool.py
+++ irctk/threadpool.py
@@ -5,7 +5,6 @@
     A thread pool to be used with plugin dispatching.
 '''
 
-import sys
 import threading
 import Queue
 import time
@@ -21,47 +20,22 @@ class Worker(threading.Thread):
     Inherits from `threading.Thread`.
     '''
     
-    def __init__(self, tasks, logger, timeout):
+    def __init__(self, tasks, logger):
         threading.Thread.__init__(self)
-        self.shutdown = False
-        self.busy = False
         self.tasks = tasks
         self.logger = logger
-        self.timeout = timeout
         self.daemon = True
         self.start()
     
-    def globaltrace(self, frame, why, arg):
-        if why == 'call':
-            return self.localtrace
-        else:
-            return None
-    
-    def localtrace(self, frame, why, arg):
-        if self.busy and (time.time() - self.start_time > self.timeout):
-            if why == 'line':
-                raise SystemExit()
-        return self.localtrace
-    
     def run(self):
-        sys.settrace(self.globaltrace)
-        while not self.shutdown:
+        while True:
             func, args, kwargs = self.tasks.get()
-            try:
-                self.busy = True
-                self.start_time = time.time()
+            try: 
                 func(*args, **kwargs)
-            except SystemExit:
-                self.logger.info('Worker terminated for misbehaving.')
-                sys.exit()
             except Exception, e:
                 error = 'Worker error: {0}'.format(e)
                 self.logger.error(error)
-            finally:
-                self.busy = False
             self.tasks.task_done()
-        else:
-            sys.exit()
 
 
 class ThreadPool(threading.Thread):
@@ -75,12 +49,11 @@ class ThreadPool(threading.Thread):
     Inherits from `threading.Thread`.
     '''
     
-    def __init__(self, min_workers, logger, max_workers=11, timeout=300, wait=0.01):
+    def __init__(self, min_workers, max_workers=11, logger=None, wait=0.01, timeout=30.0):
         threading.Thread.__init__(self)
         self.tasks = Queue.Queue()
         self.min_workers = min_workers
         self.max_workers = max_workers
-        self.timeout = timeout
         self.workers = []
         self.logger = logger
         self.wait = wait
@@ -91,42 +64,24 @@ class ThreadPool(threading.Thread):
         task = (func, args, kwargs)
         self.tasks.put(task)
     
-    def _new_worker(self):
-        self.logger.info('Starting new worker.')
-        new_worker = Worker(self.tasks, self.logger, self.timeout)
-        self.workers.append(new_worker)
-    
-    def _kill_worker(self):
-        self.logger.info('Stopping worker.')
-        i = 0
-        while i < len(self.workers):
-            worker = self.workers[i]
-            if not worker.shutdown:
-                worker.shutdown = True
-                break
-            else:
-                i += 1
+    def spawn_worker(self):
+        self.workers += Worker(self.tasks, self.logger)
     
     def run(self):
         while True:
             time.sleep(self.wait)
-
-            for i in xrange(len(self.workers) - 1):
-                if not self.workers[i].is_alive():
-                    del self.workers[i]
             
-            idle_workers = lambda : len([worker for worker in self.workers \
-                                        if not worker.busy and not worker.shutdown])
-            too_few_workers = lambda : len(self.workers) < self.min_workers
-            more_workers_ok = lambda : len(self.workers) < self.max_workers
-            too_many_workers = lambda : idle_workers() > self.min_workers
+            too_few_workers = lambda : self.number_of_workers < self.min_workers
+            more_workers_ok = lambda : self.number_of_workers < self.max_workers
+            too_many_workers = lambda : self.number_of_workers > self.min_workers
             
             if too_few_workers():
-                self._new_worker() # spawn a worker
+                self.number_of_workers += 1
+                self.spawn_worker()
             
-            if idle_workers() < self.min_workers and more_workers_ok():
-                self._new_worker()
+            if not self.tasks.empty() and more_workers_ok():
+                self.number_of_workers += 1
+                self.spwan_worker()
             
-            if too_many_workers():
-                self._kill_worker()
-
+            if self.tasks.empty() and too_many_workers():
+                self.number_of_workers -= 1
