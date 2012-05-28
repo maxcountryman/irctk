61091c0d1324077cdabfd8744b5e591a1b6090b5
Max Countryman <max.countryman@gmail.com>
2012-05-28 14:17:08 -0400

    whoops forgot: new module utils

---
+++ irctk/utils.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+    irctk.utils
+    ---------
+
+    Utility functions.
+'''
+
+
+class cached_property(object):
+    def __init__(self, func):
+        self._func = func
+        self.__name__ = func.__name__
+        self.__doc__ = func.__doc__
+
+    def __get__(self, obj, cls=None):
+        if obj is None:
+            return None
+        result = obj.__dict__[self.__name__] = self._func(obj)
+        return result
